e21d7c4ef488fbffdb5bd9f41edc39d64949bb75
killuaDev <louis.l.cullen@gmail.com>
2021-04-07 12:48:47 +0100

    Fixed numbersplitting and irregular bullet point behaviour

--- content_script.js
+++ content_script.js
@@ -16,7 +16,7 @@ console.log(word)
 var bolds = document.getElementsByClassName("fgb b clickable")  //"span" is not a class
 for (let bold of bolds) {
   var currentBold = bold.innerHTML
-  currentBold = currentBold.replace(/([\d]\. )/, '<br> $1')
+  currentBold = currentBold.replace(/([\d]*\. )/, '<br> $1')
   currentBold = currentBold.replace("~", word)
   currentBold = currentBold.replace(/(<span.*>[A-Z|Á|É|Í|Ó|Ú])/, '<span> • </span> $1')
   bold.innerHTML = currentBold
@@ -24,14 +24,14 @@ for (let bold of bolds) {
 var regulars = document.getElementsByClassName("fgb r clickable")
 for (let regular of regulars) {
   var currentRegular = regular.innerHTML
-  currentRegular = currentRegular.replace(/>\. *<\/span>/, '>. <br> </span>')
+  currentRegular = currentRegular.replace(/>[\.|?] *<\/span>/, '>. <br> </span>')
   regular.innerHTML = currentRegular
 }
 
 var italics = document.getElementsByClassName("fgb i clickable")
 for (let italic of italics) {
   var currentItalic = italic.innerHTML
-  currentItalic = currentItalic.replace(/(<span.*>[A-Z|Á|É|Í|Ó|Ú])/, '<span style="margin-left: 40px"> • </span> $1')
+  currentItalic = currentItalic.replace(/(<span.*>[A-Z|Á|É|Í|Ó|Ú|~])/, '<span style="bold"> • </span> $1')
   currentItalic = currentItalic.replace("~", word)
   italic.innerHTML = currentItalic
 }
