02011940d18c67d710aa9c51d853f192c41ca5a4
killuaDev <louis.l.cullen@gmail.com>
2021-04-09 15:02:12 +0100

    Added EID functionality

--- content_script.js
+++ content_script.js
@@ -1,24 +1,20 @@
 // Put all the javascript code here, that you want to execute after page load.
-/*let dictionary = new Map()
-dictionary.set(/( )([\d]\.)/, '\n$2')
-dictionary.set(/(\D)(\. [A-Z|Á|É|Í|Ó|Ú])/, '$1\n	$2')
-
-let regexs = new Map()
-regexs.set("numbers", new RegExp('( )([\d]\.)'))*/
-
-/*document.body.div.fgb.entry.color = "green"
-document.span.diclick.color = "green"*/
-//document.getElementsByClassName("fgb title")[0].style.backgroundColor = "green"
-//document.getElementsByClassName("fgb title")[0].innerHTML = "glac <br>"
-var word = document.getElementsByClassName("fgb title")[0].innerHTML.replace(/([\w|á|é|í|ú|ó|Á|É|Í|Ú|Ó]*).*/, '$1')
-
+// FGB
+var word = ""
+if (document.getElementsByClassName("eid current").length > 0) {
+   word = document.getElementsByClassName("eid src unclickable")[0].innerHTML
+} else {
+   word = document.getElementsByClassName("fgb title")[0].innerHTML.replace(/([\w|á|é|í|ú|ó|Á|É|Í|Ú|Ó]*).*/, '$1')
+}
+//var word = document.getElementsByClassName("fgb title")[0].innerHTML.replace(/([\w|á|é|í|ú|ó|Á|É|Í|Ú|Ó]*).*/, '$1') ?? ""
 console.log(word)
-var bolds = document.getElementsByClassName("fgb b clickable")  //"span" is not a class
+var bolds = document.getElementsByClassName("fgb b clickable")
 for (let bold of bolds) {
   var currentBold = bold.innerHTML
+  currentBold = currentBold.replace(/(<span>[^0-9|])/, '<span> • </span> $1')
   currentBold = currentBold.replace(/([\d]*\. )/, '<br> $1')
   currentBold = currentBold.replace(/~/g, word)
-  currentBold = currentBold.replace(/(<span.*>[a-z|A-Z|á|é|í|ú|ó|Á|É|Í|Ú|Ó|~])/, '<span> • </span> $1')
+
   bold.innerHTML = currentBold
 }
 var regulars = document.getElementsByClassName("fgb r clickable")
@@ -34,4 +30,16 @@ for (let italic of italics) {
   currentItalic = currentItalic.replace(/(<span.*>[A-Z|Á|É|Í|Ó|Ú|~])/, '<span style="bold"> • </span> $1')
   currentItalic = currentItalic.replace("~", word)
   italic.innerHTML = currentItalic
+}
+
+///
+
+// EID
+var entries = document.getElementsByClassName("eid entry")
+for (let entry of entries) {
+  var currentEntry = entry.innerHTML
+  currentEntry = currentEntry.replace(/(<span class="eid sense unclickable">)/g, '<br><br> $1')
+  currentEntry = currentEntry.replace(/(<span class="eid subsense unclickable")/g, '<br> $1 style="margin-left: 20px"')
+  currentEntry = currentEntry.replace(/(<span class="eid src clickable")/g, '<br> $1 style="margin-left:40px"')
+  entry.innerHTML = currentEntry
 }
