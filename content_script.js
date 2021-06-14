0601ccb6e3260b6af2b591fb3ba89829cfa7d3ca
killuaDev <louis.l.cullen@gmail.com>
2021-06-14 19:37:05 +0100

    fixed the bottom area, made it so that ~s change to the correct words

--- content_script.js
+++ content_script.js
@@ -6,25 +6,28 @@ if (document.getElementsByClassName("eid current").length > 0) {
 } else {
    word = document.getElementsByClassName("fgb title")[0].innerHTML.replace(/([\w|á|é|í|ú|ó|Á|É|Í|Ú|Ó]*).*/, '$1')
 }
-//var word = document.getElementsByClassName("fgb title")[0].innerHTML.replace(/([\w|á|é|í|ú|ó|Á|É|Í|Ú|Ó]*).*/, '$1') ?? ""
+
+// Exact Matches
+var topArea = document.getElementsByClassName("dir obverse exacts")[0]
+
 console.log(word)
-var bolds = document.getElementsByClassName("fgb b clickable")
+var bolds = topArea.getElementsByClassName("fgb b clickable")
 for (let bold of bolds) {
   var currentBold = bold.innerHTML
   currentBold = currentBold.replace(/(<span>[^0-9|])/, '<span> • </span> $1')
   currentBold = currentBold.replace(/([\d]*\. )/, '<br> $1')
-  currentBold = currentBold.replace(/~/g, word)
+  currentBold = currentBold.replace(/~/g, word) 
 
   bold.innerHTML = currentBold
 }
-var regulars = document.getElementsByClassName("fgb r clickable")
+var regulars = topArea.getElementsByClassName("fgb r clickable")
 for (let regular of regulars) {
   var currentRegular = regular.innerHTML
   currentRegular = currentRegular.replace(/>[\.|?] *<\/span>/, '>. <br> </span>')
   regular.innerHTML = currentRegular
 }
 
-var italics = document.getElementsByClassName("fgb i clickable")
+var italics = topArea.getElementsByClassName("fgb i clickable")
 for (let italic of italics) {
   var currentItalic = italic.innerHTML
   currentItalic = currentItalic.replace(/(<span.*>[A-Z|Á|É|Í|Ó|Ú|~])/, '<span style="bold"> • </span> $1')
@@ -32,7 +35,16 @@ for (let italic of italics) {
   italic.innerHTML = currentItalic
 }
 
-///
+// Phrases in FGB
+var bottomArea = document.getElementsByClassName("dir obverse")[2]
+var examples = bottomArea.getElementsByClassName("ex")
+for (let example of examples) {
+  word = example.getElementsByTagName("a")[0].innerHTML.replace(/([^<]*) <span.*/, '$1')
+  console.log(word)
+  example.innerHTML = example.innerHTML.replace(/~/, word)
+  
+
+}
 
 // EID
 var entries = document.getElementsByClassName("eid entry")
